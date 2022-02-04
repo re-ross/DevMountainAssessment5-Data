4048700b1dbc207878b73ec6e041929d794fb309
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-04 09:43:00 -0600

    finished part 1 and part 2

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #this line is opening um-server-01.txt and saving it to a variable to be access later.
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #this line is creating a function via def declaration, naming function sales_reports
+    for line in log_file: # setting up a for loop for every row of data in the linked file
+        line = line.rstrip() #this sets value of line to line.rstrip() which removes any trailing characters
+        day = line[0:3] # declaring variable named day, which is the characters of line from index 0 to index 3
+        if day == "Tue": # if statement checking if variable day is equal to string of 'Tue'
+            print(line) # if so print that line it came cfrom
 
 
-sales_reports(log_file)
+sales_reports(log_file) #this is calling the created function over the linked file / database
--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
+const { CONNECTION_STRING } = process.env;
+const Sequelize = require("sequelize");
 
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = {
-    seed: (req, res) => {
-        sequelize.query(`
+  seed: (req, res) => {
+    sequelize
+      .query(
+        `
             drop table if exists cities;
             drop table if exists countries;
 
@@ -209,9 +222,12 @@ module.exports = {
             ('Yemen'),
             ('Zambia'),
             ('Zimbabwe');
-        `).then(() => {
-            console.log('DB seeded!')
-            res.sendStatus(200)
-        }).catch(err => console.log('error seeding DB', err))
-    }
-}
+        `
+      )
+      .then(() => {
+        console.log("DB seeded!");
+        res.sendStatus(200);
+      })
+      .catch((err) => console.log("error seeding DB", err));
+  },
+};
